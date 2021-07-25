ec3608527b9dae7a17066bac08d80893ff49cc2a
Pavel Matveev <p.matveev@galament.ru>
2021-07-25 15:15:05 +0400

    Lesson 17: Params of methods

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,23 +1,25 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Роман";
-        person1.age = 50;
+        person1.setNameAndAge("Роман", 50);
 
+        String s1 = "Вова";
         Person person2 = new Person();
-        person2.name = "Вова";
-        person2.age = 20;
+        person2.setNameAndAge(s1, 20);
 
-        int year1 = person1.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Первому человеку до пенсии " + year1 + " лет");
-        System.out.println("Второму человеку до пенсии " + year2 + " лет");
+        person1.speak();
+        person2.speak();
     }
 }
 
 class Person {
     String name;
     int age;
+
+    void setNameAndAge(String user_name, int user_age) {
+        name = user_name;
+        age = user_age;
+    }
 
     int calculateYearsToRetirement() {
         int years = 65 - age;
