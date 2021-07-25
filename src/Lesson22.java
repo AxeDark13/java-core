be2ee8283ac2200863a382a59a81a29cbe53ec80
Pavel Matveev <p.matveev@galament.ru>
2021-07-25 17:30:14 +0400

    Lesson 22: Key-word 'final'

---
+++ src/Lesson22.java
@@ -0,0 +1,11 @@
+public class Lesson22 {
+    public static void main(String[] args) {
+        final int X = 10;
+        System.out.println(X);
+        //X = 5; - error
+    }
+}
+
+class Test {
+    public static final int CONSTANT = 10;
+}
