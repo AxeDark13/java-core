a82d9dac81ac4de464c58f97ac048de5edb060ea
Pavel Matveev <p.matveev@galament.ru>
2021-07-25 18:39:22 +0400

    Lesson 23: StringBuilder

---
+++ src/Lesson23.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class Lesson23 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println(sb.toString());
+        sb.append(" my").append(" friend");
+        System.out.println(sb.toString());
+    }
+}
