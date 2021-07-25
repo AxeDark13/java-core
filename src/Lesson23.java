b6c827e626ab77557e05047d5bd3f0aad14a214b
Pavel Matveev <p.matveev@galament.ru>
2021-07-25 18:46:33 +0400

    Lesson 24: Strings formating

--- src/Lesson23.java
+++ src/Lesson23.java
@@ -2,9 +2,10 @@ import java.util.Locale;
 
 public class Lesson23 {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Hello");
-        System.out.println(sb.toString());
-        sb.append(" my").append(" friend");
-        System.out.println(sb.toString());
+        System.out.printf("String %.2f\n", 53.4112);
+        System.out.printf("String %.2f\n", 53.41);
+        System.out.printf("String %.2f\n", 53.4112);
+        System.out.printf("String %.2f\n", 53.4112124124);
+        System.out.printf("String %.2f\n", 53.337);
     }
 }
