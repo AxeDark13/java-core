35db29ba4a441f9f6b9d8e36371cb9a90c8f5dc8
Pavel Matveev <p.matveev@galament.com>
2021-07-26 15:03:53 +0400

    Lesson 39: Exceptions (Part 3. Checked and unchecked exceptions)

---
+++ src/Lesson39/Exceptions3.java
@@ -0,0 +1,16 @@
+package Lesson39;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exceptions3 {
+    public static void main(String[] args) {
+        //Checked Exception(Compile time exception)
+        //Unchecked (Runtime exception)
+
+        //int a = 1/ 0; - Runtime exception
+        //String name = null; name.length(); - Runtime exception
+        //int[] arr = new int[2]; System.out.println(arr[2]); - Runtime exception
+    }
+}
