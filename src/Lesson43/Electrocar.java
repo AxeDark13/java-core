c2c54e23389ea5426cd409267396a491452624ee
Pavel Matveev <p.matveev@galament.com>
2021-07-26 15:44:22 +0400

    Lesson 43: Nested classes

---
+++ src/Lesson43/Electrocar.java
@@ -0,0 +1,39 @@
+package Lesson43;
+
+public class Electrocar {
+    private int id;
+
+    //Вложенный не статический класс
+    private class Motor {
+        public void startMotor() {
+            System.out.println("Motor " + id + " is starting...");
+        }
+    }
+
+    //Вложенный статический класс
+    public static class Battery {
+        public void charge() {
+            System.out.println("Battery is charging...");
+            //using 'id' - wrong
+        }
+    }
+
+    public Electrocar(int id) {
+        this.id = id;
+    }
+
+    public void start() {
+        Motor motor = new Motor();
+        motor.startMotor();
+
+        final int x = 1;
+        class SomeClass {
+            public void someMethod() {
+                System.out.println(x);
+                System.out.println(id);
+            }
+        }
+
+        System.out.println("Electrocar " + id + " is starting...");
+    }
+}
