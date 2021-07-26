d9d2cb5a50c5fb3a471156242e0573c296783586
Pavel Matveev <p.matveev@galament.com>
2021-07-26 12:36:57 +0400

    Lesson 32: Wrapper-classes

---
+++ src/Lesson31/Test.java
@@ -0,0 +1,11 @@
+package Lesson31;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+        Integer x2 = 123; // <=> Integer x2 = new Integer(123)
+
+        Integer.parseInt("123");
+        Boolean.TRUE;
+    }
+}
