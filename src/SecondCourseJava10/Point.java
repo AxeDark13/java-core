09a69a88d60e3b1b4a73b8182a8b60b74c4338b3
Pavel Matveev <p.matveev@galament.com>
2021-08-05 16:30:43 +0400

    Second Course Java 10: Drawing points in 3D

---
+++ src/SecondCourseJava10/Point.java
@@ -0,0 +1,13 @@
+package SecondCourseJava10;
+
+public class Point {
+    float x;
+    float y;
+    float z;
+
+    public Point(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
