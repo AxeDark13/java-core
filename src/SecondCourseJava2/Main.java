019dc08b561155635072296eadbb48ecfc342d11
Pavel Matveev <p.matveev@galament.com>
2021-08-06 12:42:23 +0400

    Temp. Work in progress.

--- src/SecondCourseJava2/Main.java
+++ src/SecondCourseJava2/Main.java
@@ -3,22 +3,36 @@ package SecondCourseJava2;
 import java.util.ArrayList;
 
 public class Main {
+    public static volatile boolean isStopped = false;
+
     public static void main(String[] args) {
         BlockingQueue queue = new BlockingQueue();
         Thread worker = new Thread(new Runnable() {
             @Override
             public void run() {
-                while(true) {
-                    Runnable task = queue.get();
-                    task.run();
+                while(!Thread.currentThread().isInterrupted()) {
+                    Runnable task = null;
+                    try {
+                        task = queue.get();
+                        task.run();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
             }
         });
         worker.start();
 
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < 2; i++) {
             queue.put(getTask());
         }
+
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        worker.interrupt();
     }
 
     public static Runnable getTask() {
@@ -40,13 +54,9 @@ public class Main {
     static class BlockingQueue {
         ArrayList<Runnable> tasks = new ArrayList<>();
 
-        public synchronized  Runnable get() {
+        public synchronized  Runnable get() throws InterruptedException {
             while(tasks.isEmpty()) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                wait();
             }
             Runnable task = tasks.get(0);
             tasks.remove(task);
